13c7060b02e07e959a50d3b42c6ee68fe69bffb1
刘炎 <1159660811@qq.com>
2017-03-10 09:36:48 +0800

    测试Jquery提交Form表单.

---
+++ youyin_MB/src/main/java/liuyan/youyin/controller/PostTest.java
@@ -0,0 +1,21 @@
+package liuyan.youyin.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostTest
+ */
+public class PostTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("password"));
+	}
+
+}
